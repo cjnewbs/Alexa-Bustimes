29da43d4578cdc91db2008ed86fb3973e9c1a6c4
Craig Newbury <craig@newbury.me>
2016-10-29 15:24:49 +0100

    Converted into class to simplify code

--- index.php
+++ index.php
@@ -1,168 +1,176 @@
 <?php
-require 'config.php';
+class BusTimes
+{
+    protected $requestBody;
 
-// get POST body from request and extract into an array
-$body = file_get_contents('php://input');
-$body = json_decode($body, true);
+    protected $routeSlot;
 
-// get slots from request
-if (isset($body['request']['intent']['slots']['route']['value'])) {
-    $route = $body['request']['intent']['slots']['route']['value'];
-}
-if (isset($body['request']['intent']['slots']['count']['value'])) {
-    $count = $body['request']['intent']['slots']['count']['value'];
-}
+    protected $countSlot;
 
-// load HTML from real-time page
-$realTimeHTML = file_get_contents($realTimeInfoURL);
+    protected $buses;
 
-// Load all text in TD tags from web page
-$dom = new DOMDocument();
-$dom->loadHTML($realTimeHTML);
-$cells = $dom->getElementsByTagName('td');
+    protected $speechString;
 
-// strip empty elements and re-order index
-$busses = toBusArray($cells);
+    public function __construct()
+    {
+        // get POST body from request and extract into an array
+        $body = file_get_contents('php://input');
+        file_put_contents('alexa.log', $body);
+        $this->requestBody = json_decode($body, true);
+    }
 
-// get speech text based on slots supplied
-if (isset($route) && isset($count)) {
-    //user asked for next {x} busses on {y} route
-    $filteredBusses = getNextBussesByRouteAndCount($busses, $route, $count);
-    $speechString = loadSpeech($filteredBusses);
+    protected function getRequestSlots()
+    {
+        if (isset($this->requestBody['request']['intent']['slots']['route']['value'])) {
+            $this->routeSlot = $this->requestBody['request']['intent']['slots']['route']['value'];
+        }
+        if (isset($this->requestBody['request']['intent']['slots']['count']['value'])) {
+            $this->countSlot = $this->requestBody['request']['intent']['slots']['count']['value'];
+        }
+    }
 
-} elseif (isset($route)) {
-    // user asked for next number {x} bus
-    $filteredBusses = getNextBussesByRoute($busses, $route);
-    $speechString = loadSpeech($filteredBusses);
+    protected function extractBusData($html)
+    {
+        // Load all text in TD tags from web page
+        $dom = new DOMDocument();
+        $dom->loadHTML($html);
+        $cells = $dom->getElementsByTagName('td');
 
-} elseif (isset($count)) {
-    // user asked for next {x} busses
-    $filteredBusses = getNextBussesByCount($busses, $count);
-    $speechString = loadSpeech($filteredBusses);
+        // create new array and strip any empty cells
+        $newArray = array();
+        foreach ($cells as $cell) {
+            if ($cell->textContent != "\xC2\xA0") {
+                $newArray[] = $cell->textContent;
+            }
+        }
+        $count = count($newArray);
 
+        $buses = array();
 
-} else {
-    // user asked for next bus
-    $speechString = getNextBus($busses);
-}
+        $i = 0;
+        while ($i < $count) {
+            $buses[] = array($newArray[$i++], $newArray[$i++], $newArray[$i++]);
+        }
+        $this->buses = $buses;
+    }
 
-$responseJSON = '{
+    protected function filterBusesByRoute()
+    {
+        // first filter by route
+        $routeResult = array();
+        foreach ($this->buses as $bus) {
+            if ($bus[0] == $this->routeSlot) {
+                $routeResult[] = $bus;
+            }
+        }
+        $this->buses = $routeResult;
+        return $this;
+    }
+
+    protected function filterBusesByCount()
+    {
+        if (count($this->buses) > $this->countSlot) {
+            $countResult = $this->buses;
+            $this->buses = null;
+            $this->buses = array();
+            for ($i = 0; $i < $this->countSlot; $i++) {
+                $this->buses[] = $countResult[$i];
+            }
+        }
+        return $this;
+    }
+
+    protected function filterBusesByNextBus() {
+        $nextBus = [$this->buses[0]];
+        $this->buses = null;
+        $this->buses = $nextBus;
+        return $this;
+    }
+
+    protected function renderSpeech()
+    {
+        if (!empty($this->buses)) {
+            $this->speechString = 'The next bus is';
+
+            $multipleRows = false;
+
+            foreach ($this->buses as $bus) {
+                if ($multipleRows === true) {
+                    $this->speechString .= ', followed by';
+                }
+                $this->speechString .= ' the number ' . $bus[0] . ' to ' . $bus[1] . ' ';
+                $dueTime = $bus[2];
+                if ($dueTime == 'Due') {
+                    // Bus due now
+                    $this->speechString .= 'which is due now';
+                } elseif (strpos($dueTime, 'Mins') !== false) {
+                    // Bus due in x mins
+                    $this->speechString .= 'which is due in ' . $dueTime;
+                } else {
+                    // Bus due at specific time
+                    $this->speechString .= 'which is due at ' . $dueTime;
+                }
+                $multipleRows = true;
+            }
+        } else {
+            $this->speechString = 'Sorry, there were no buses matching your criteria.';
+        }
+    }
+
+    public function run()
+    {
+        require 'config.php';
+
+        // load HTML from real-time page
+        $realTimeHTML = file_get_contents($realTimeInfoURL);
+        $this->extractBusData($realTimeHTML);
+
+        $this->getRequestSlots();
+
+        // get speech text based on slots supplied
+        if (isset($this->countSlot) && isset($this->routeSlot)) {
+
+            //user asked for next {x} buses on {y} route
+            $this   ->filterBusesByRoute()
+                    ->filterBusesByCount()
+                    ->renderSpeech();
+
+        } elseif (isset($this->routeSlot)) {
+
+            // user asked for next number {x} bus
+            $this   ->filterBusesByRoute()
+                    ->renderSpeech();
+
+        } elseif (isset($this->countSlot)) {
+
+            // user asked for next {x} buses
+            $this   ->filterBusesByCount()
+                    ->renderSpeech();
+
+        } else {
+            // user asked for next bus
+
+            $this   ->filterBusesByNextBus()
+                    ->renderSpeech();
+        }
+
+        $responseJSON = '{
     "version": "1.0",
     "response": {
         "outputSpeech": {
             "type": "PlainText",
-             "text": "'.$speechString.'"
+             "text": "'.$this->speechString.'"
         },
         "shouldEndSession": true
     }
 }';
 
-$responseLen = strlen($responseJSON);
-header('Content-Type: application/json;charset=UTF-8');
-header('Content-Length: '.$responseLen);
-echo $responseJSON;
+        $responseLen = strlen($responseJSON);
+        header('Content-Type: application/json;charset=UTF-8');
+        header('Content-Length: '.$responseLen);
+        echo $responseJSON;
 
-// generate the speech
-function loadSpeech($busses) {
-
-    if (!empty($busses)) {
-        $outputString = 'The next bus is';
-
-        $multipleRows = false;
-
-        foreach ($busses as $bus) {
-            if ($multipleRows === true) {
-                $outputString .= ', followed by';
-            }
-            $outputString .= ' the number ' . $bus[0] . ' to ' . $bus[1] . ' ';
-            $dueTime = $bus[2];
-            if ($dueTime == 'Due') {
-                // Bus due now
-                $outputString .= 'which is due now';
-            } elseif (strpos($dueTime, 'Mins') !== false) {
-                // Bus due in x mins
-                $outputString .= 'which is due in ' . $dueTime;
-            } else {
-                // Bus due at specific time
-                $outputString .= 'which is due at ' . $dueTime;
-            }
-            $multipleRows = true;
-        }
-    } else {
-        $outputString = 'Sorry, there were no busses matching your criteria.';
-    }
-    return $outputString;
-
-}
-
-function getNextBussesByRouteAndCount($busses, $route, $count) {
-
-    // first filter by route
-    $routeResult = array();
-    foreach ($busses as $bus) {
-        if ($bus[0] == $route) {
-            $routeResult[] = $bus;
-        }
-    }
-
-    // next filter by count
-    if (count($routeResult) > $count) {
-        $countResult = array();
-        $i = 0;
-        while ($count > count($countResult)) {
-            $countResult[] = $routeResult[$i];
-            $i++;
-        }
-        return $countResult;
-    } else {
-        return $routeResult;
     }
 }
-
-function getNextBussesByCount($busses, $count)
-{
-    // filter by count
-    if (count($busses) > $count) {
-        $countResult = array();
-        for ($i = 0; $i < $count; $i++) {
-            $countResult[] = $countResult[$i];
-        }
-        return $countResult;
-    } else {
-        return $busses;
-    }
-}
-
-function getNextBussesByRoute($busses, $route) {
-    // first filter by route
-    $routeResult = array();
-    foreach ($busses as $bus) {
-        if ($bus[0] == $route) {
-            $routeResult[] = $bus;
-        }
-    }
-    return $routeResult;
-}
-
-function getNextBus($busses) {
-    return loadSpeech([$busses[0]]);
-}
-
-function toBusArray($cells) {
-    // create new array and strip any empty cells
-    $newArray = array();
-    foreach ($cells as $cell) {
-        if ($cell->textContent != "\xC2\xA0") {
-            $newArray[] = $cell->textContent;
-        }
-    }
-    $count = count($newArray);
-
-    $busses = array();
-
-    $i = 0;
-    while ($i < $count) {
-        $busses[] = array($newArray[$i++], $newArray[$i++], $newArray[$i++]);
-    }
-    return $busses;
-}
+$alexa = new BusTimes();
+$alexa->run();
